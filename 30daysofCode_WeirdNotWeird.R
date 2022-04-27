f51c9f73cb201440379319778cc3909e13355025
Sarah Ewing <101220213+sarahewing1@users.noreply.github.com>
2022-04-27 19:29:14 -0400

    Add files via upload

---
+++ 30daysofCode_WeirdNotWeird.R
@@ -0,0 +1,15 @@
+
+x <- 3
+y<-as.integer(x)
+if(y%%2==1){
+  print("Weird")
+  }else if(y%%2==0)
+  if(y>=2&&y<=5){
+    print("Not Weird")
+    }else if(y>=6 && y<=20){
+    print("Weird")
+      }else if(y>20){
+    print("Not Weird")}
+  
+x <- 24
+  
